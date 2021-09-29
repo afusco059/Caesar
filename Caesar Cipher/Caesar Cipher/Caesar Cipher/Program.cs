afbafcb05eb1e2c2a4d787b865ee288b21683151
afusco059 <70164839+afusco059@users.noreply.github.com>
2021-09-29 09:02:42 -0400

    Add files via upload

---
+++ Caesar Cipher/Caesar Cipher/Caesar Cipher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesar_Cipher
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            Console.WriteLine("type a message to encrypt it.");
+            string message = Console.ReadLine();
+            char[] secretMessage = message.ToCharArray();
+
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+            int i;
+            for (i = 0; i < secretMessage.Length; i++)
+            {
+                char secretItem = secretMessage[i];
+                int index = Array.IndexOf(alphabet, secretItem);
+                int letterPosition = (index += 3) % 26;
+                char encryptedCharacter = alphabet[letterPosition]; // this line now gets the encrypted character as a CHAR
+                encryptedMessage[i] = encryptedCharacter; // so we can just pop it directly into the encrypted message!
+            }
+
+            string superSecretMessage = String.Join("", encryptedMessage);
+            Console.Write(superSecretMessage);
+        }
+    }
+}
